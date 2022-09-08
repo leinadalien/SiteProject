a0684343d14d717502c5eec2b4ede72afb872477
leinadalien <danila.box@tut.by>
2022-09-09 01:04:00 +0300

    some commit

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+class Publication(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
